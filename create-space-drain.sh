d61cacfc4ae6e5c8d8f99d200f5c4e82fdb557f8
Jin-Sun-tts <jin.sun@gsa.gov>
2024-01-12 14:28:41 -0500

    debug error

--- create-space-drain.sh
+++ create-space-drain.sh
@@ -34,6 +34,7 @@ prefix=${3:-logstack}
 echo "test... downloading..."
 curl -L -o /github/home/.cf/plugins/drain-plugin https://github.com/cloudfoundry/cf-drain-cli/releases/download/v2.0.0/cf-drain-cli-linux --insecure &&
 echo "test... finish download..."
+pwd
 cd /github/home/.cf/plugins/
 ls -l
 ldd drain-plugin
